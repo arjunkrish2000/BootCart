f0064d5e2873523e93cf13c80728795050c0a477
Arjun Krishna J G <44795912+arjunkrish2000@users.noreply.github.com>
2022-12-21 11:41:32 +0530

    Navbar Updated

--- Client/BootCart/src/app/productmaster/productmaster.module.ts
+++ Client/BootCart/src/app/productmaster/productmaster.module.ts
@@ -10,6 +10,7 @@ import { AddProductComponent } from './add-product/add-product.component';
 import { FormsModule } from '@angular/forms';
 import { CookieService } from 'ngx-cookie-service/public-api';
 import { AddProductSpecificationComponent } from './add-product-specification/add-product-specification.component';
+import { PublicModule } from '../public/public.module';
 
 
 @NgModule({
@@ -25,7 +26,7 @@ import { AddProductSpecificationComponent } from './add-product-specification/ad
     ProductmasterRoutingModule,
     RouterModule,
     FormsModule,
-    
+    PublicModule
   ]
 })
 export class ProductmasterModule { }
